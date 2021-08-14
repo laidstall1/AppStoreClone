cdc82995fbeb6e03979eb6599e570672998f09e9
Oluwatoni <laidstall@gmail.com>
2021-08-14 15:32:22 +0100

    create a searchAppsController, an AppsSearchCell which configures the SearchController's CollectionView and add a SearchAppsViewModel and Search Model which populates the AppSearchCell with data from the appStore API

---
+++ AppStoreClone/AppStoreClone/Controllers/SearchAppsController.swift
@@ -0,0 +1,67 @@
+//
+//  AppsSearchController.swift
+//  AppStoreClone
+//
+//  Created by mac on 12/08/2021.
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class SearchAppsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    let viewModel = AppSearchViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        self.collectionView!.register(SearchResultCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        configureSearchBar()
+    }
+
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func beginSearch(with searchText: String) {
+        viewModel.beginSearchText(searchText) {
+            DispatchQueue.main.async { [weak self] in
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func configureSearchBar() {
+        definesPresentationContext = true
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "App Store"
+    }
+    
+    
+    // MARK: UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.appResult.count
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SearchResultCell
+        cell.configure(with: viewModel.appResult[indexPath.item])
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 350)
+    }
+}
+
+extension SearchAppsController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        beginSearch(with: searchText)
+    }
+}
